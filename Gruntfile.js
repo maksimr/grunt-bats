d8b46d02ebdf20c6383d4fc30427600f43375999
Maksim Ryzhikov <rv.maksim@gmail.com>
2013-09-28 17:13:47 +0400

    chor: add autorelease task

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,6 +28,25 @@ module.exports = function(grunt) {
             tests: ['test/*_test.bats']
         },
 
+        'npm-contributors': {
+            options: {
+                commitMessage: 'chore: update contributors'
+            }
+        },
+
+        bump: {
+            options: {
+                commitMessage: 'chore: release v%VERSION%',
+                pushTo: 'origin'
+            }
+        },
+
+        'auto-release': {
+            options: {
+                checkTravisBuild: true
+            }
+        },
+
         watch: {
             files: '<%= jshint.all %>',
             tasks: 'test'
@@ -40,6 +59,15 @@ module.exports = function(grunt) {
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
     grunt.registerTask('test', ['bats']);
+    grunt.registerTask('release', 'Bump the version and publish to NPM.', function(type) {
+        type = type || 'patch';
+
+        grunt.task.run([
+            'npm-contributors',
+            'bump:' + type,
+            'npm-publish'
+        ]);
+    });
 
     // By default, lint and run all tests.
     grunt.registerTask('default', ['jshint', 'test']);
